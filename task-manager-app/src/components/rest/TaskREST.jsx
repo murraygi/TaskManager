74c86b9f71b6aad3153da2bca32e867f81141481
murraygi <murraygiudici@gmail.com>
2025-02-26 22:25:37 +0000

    Created duplicate components for GraphQL. Renamed and reordered exisiting components. Created useTasks files for each API to abstract fetch calls as custom hooks.

---
+++ task-manager-app/src/components/rest/TaskREST.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+
+function Task(props) {
+  return (
+    <div className={`task ${props.completed ? "completed" : ""}`}>
+      <div className="task-header">
+        <input
+          type="checkbox"
+          checked={props.completed}
+          onChange={() => props.onToggleComplete(props.id)}
+        />
+        <h2>{props.title}</h2>
+      </div>
+      <p className="taskContent">{props.content}</p>
+      <div className="task-footer">
+        <p className="taskPriority">Priority: {props.priority || "Not set"}</p>
+          <button onClick={props.onEdit}>
+            <EditIcon />
+          </button>
+          <button onClick={() => props.onDelete(props.id)}>
+            <DeleteIcon />
+          </button>
+        </div>
+      </div>
+  );
+}
+
+export default Task;
